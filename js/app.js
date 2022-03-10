3cfb4e0ff6544b84c5d932a4ed2fe3bba1765500
Jason Wilson <jasonstimphil@gmail.com>
2022-03-10 16:34:40 -0500

    question2-7 added functions. forgot to do acp them individually.

--- js/app.js
+++ js/app.js
@@ -24,112 +24,138 @@ function questainOne(){
 questainOne();
 
 // questaion two
-let answerTwo = prompt('Do i have a PHD').toUpperCase();
+function questionTwo(){
 
-if(answerTwo === 'Y' || answerTwo === 'YES'){
-  //console.log('no, you are wrong');
-  alert('no, you are wrong');
-
-} else if(answerTwo === 'N' || answerTwo === 'NO'){
-  //console.log('Youre right!');
-  alert('Youre right!');
-  score++;
-
+  let answerTwo = prompt('Do i have a PHD').toUpperCase();
+  
+  if(answerTwo === 'Y' || answerTwo === 'YES'){
+    //console.log('no, you are wrong');
+    alert('no, you are wrong');
+  
+  } else if(answerTwo === 'N' || answerTwo === 'NO'){
+    //console.log('Youre right!');
+    alert('Youre right!');
+    score++;
+  
+  }
 }
+questionTwo();
+
 // Questain three
-let answerThree = prompt('Is my name Abdulahi?').toUpperCase();
-
-if(answerThree === 'Y' || answerThree === 'YES'){
-  //console.log('Youre right!');
-  alert('Youre right!');
-  score++;
-
-} else if(answerThree === 'N' || answerThree === 'NO'){
-  //console.log('no, you are wrong');
-  alert('no, you are wrong');
-
+function questionThree(){
+  
+  let answerThree = prompt('Is my name Abdulahi?').toUpperCase();
+  
+  if(answerThree === 'Y' || answerThree === 'YES'){
+    //console.log('Youre right!');
+    alert('Youre right!');
+    score++;
+  
+  } else if(answerThree === 'N' || answerThree === 'NO'){
+    //console.log('no, you are wrong');
+    alert('no, you are wrong');
+  
+  }
 }
+questionThree();
+
 // Questain Four
-let answerFour = prompt('Is 201 fun?').toUpperCase();
-
-if(answerFour === 'Y' || answerFour === 'YES'){
-  //console.log('Youre right!');
-  alert('Youre right!');
-  score++;
-
-} else if(answerFour === 'N' || answerFour === 'NO'){
-  //console.log('no, you are wrong');
-  alert('no, you are wrong');
-
+function questionFour(){
+  
+  let answerFour = prompt('Is 201 fun?').toUpperCase();
+  
+  if(answerFour === 'Y' || answerFour === 'YES'){
+    //console.log('Youre right!');
+    alert('Youre right!');
+    score++;
+  
+  } else if(answerFour === 'N' || answerFour === 'NO'){
+    //console.log('no, you are wrong');
+    alert('no, you are wrong');
+  
+  }
 }
+questionFour();
+
+
 // questaion five
-let answerFive = prompt('Do i have a son?').toUpperCase();
+function questionFive(){
 
-if(answerFive === 'Y' || answerFive === 'YES'){
-  //console.log('no, you are wrong');
-  alert('no, you are wrong');
-
-} else if(answerFive === 'N' || answerFive === 'NO'){
-  //console.log('Youre right!');
-  alert('Youre right!');
-  score++;
-
+  let answerFive = prompt('Do i have a son?').toUpperCase();
+  
+  if(answerFive === 'Y' || answerFive === 'YES'){
+    //console.log('no, you are wrong');
+    alert('no, you are wrong');
+  
+  } else if(answerFive === 'N' || answerFive === 'NO'){
+    //console.log('Youre right!');
+    alert('Youre right!');
+    score++;
+  
+  }
 }
+questionFive();
+
 
 alert(`${userName} thank you for anwsering the questains !`);
 
 // questain six
+function QuestionSix(){
 
-let userGuess = prompt('Enter a number between 1 & 10');
-let rightAnswer = 5;
-let tries = 3;
-
-for (let i = 0; i < tries; i++) {
-  while (~~userGuess < 1 || ~~userGuess > 10) {
-    userGuess = prompt('Please enter a number between 1 & 10');
-  }
-  if (~~userGuess === rightAnswer) {
-    alert('WINNER WINNER CHICKEN DINNER (unless ur vegan)');
-    score++;
-    break;
-  } else if (~~userGuess < rightAnswer){
-    userGuess = prompt('WRONG THE NUMBER IS HIGHER');
-
-  } else if (~~userGuess > rightAnswer) {
-    userGuess = prompt('WRONG THE NUMBER IS LOWER');
-  }
-  if (i === 2) {
-    alert(`Sorry you failed, the right answer was ${rightAnswer} !`);
-  }
-}
-
-// questain Seven
-
-let myFavColors = ['orange','indigo','cyan','khaki'];
-let correct = false;
-let answer = null;
-for (let i = 0; i < 6; i++) {
-  let colorAnsw = prompt('Can you guess my favorite color ?').toLowerCase();
-
-  for (let j = 0; j < myFavColors.length; j++){
-    if (colorAnsw === myFavColors[j]) {
-      correct = true;
+  let userGuess = prompt('Enter a number between 1 & 10');
+  let rightAnswer = 5;
+  let tries = 3;
+  
+  for (let i = 0; i < tries; i++) {
+    while (~~userGuess < 1 || ~~userGuess > 10) {
+      userGuess = prompt('Please enter a number between 1 & 10');
+    }
+    if (~~userGuess === rightAnswer) {
+      alert('WINNER WINNER CHICKEN DINNER (unless ur vegan)');
       score++;
-      i = 6;
-      answer = colorAnsw;
+      break;
+    } else if (~~userGuess < rightAnswer){
+      userGuess = prompt('WRONG THE NUMBER IS HIGHER');
+  
+    } else if (~~userGuess > rightAnswer) {
+      userGuess = prompt('WRONG THE NUMBER IS LOWER');
+    }
+    if (i === 2) {
+      alert(`Sorry you failed, the right answer was ${rightAnswer} !`);
     }
   }
+}
+QuestionSix();
+
+// questain Seven
+function questionSeven(){
+
+  let myFavColors = ['orange','indigo','cyan','khaki'];
+  let correct = false;
+  let answer = null;
+  for (let i = 0; i < 6; i++) {
+    let colorAnsw = prompt('Can you guess my favorite color ?').toLowerCase();
   
+    for (let j = 0; j < myFavColors.length; j++){
+      if (colorAnsw === myFavColors[j]) {
+        correct = true;
+        score++;
+        i = 6;
+        answer = colorAnsw;
+      }
+    }
+    
+  }
+  if (!correct){
+    alert('you are wrong');
+  
+  }else {
+    alert(`you are right my favorite color is ${answer}`);
+  
+  }
+  alert(`My favorite colors are ${myFavColors}`);
+  alert(`Your score is ${score} out of 7 !`);
 }
-if (!correct){
-  alert('you are wrong');
-
-}else {
-  alert(`you are right my favorite color is ${answer}`);
-
-}
-alert(`My favorite colors are ${myFavColors}`);
-alert(`Your score is ${score} out of 7 !`);
-
+questionSeven();
 
 
